c3cf064c4e2f8d91f7343d356946be8178e0b45c
basfroman <basfroman@gmail.com>
2021-08-05 10:32:53 -0700

    Separate memory in a process
    In multiprocessing, each worker has its own memory. The memory is not \shared like in threading.
    
    We create a worker to which we pass the global data list. We add additional values to the list in the worker but the original list in the main process is not modified.

---
+++ own_memory_space.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+from multiprocessing import Process, current_process
+
+data = [1, 2]
+
+
+def fun():
+
+    global data
+
+    data.extend((3, 4, 5))
+    print(f'Result in {current_process().name}: {data}')
+
+
+def main():
+
+    worker = Process(target=fun)
+    worker.start()
+    worker.join()
+
+    print(f'Result in main: {data}')
+
+
+if __name__ == '__main__':
+
+    main()
